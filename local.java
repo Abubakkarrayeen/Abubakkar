90fd79358c8ad044d2ab4f41d40c4f6c9bae5fa6
MD. ABUBAKKAR RAYEEN <md.abubakkarrayeen@MDs-MacBook-Air.local>
2025-04-16 14:46:35 +0545

    added

--- local.java
+++ local.java
@@ -1,13 +1,21 @@
 public class local {
 
+    int age;
+    String name;
+    static int companycode = 78;
+
     void display(){
         int age = 8;
-        System.out.println(age);
+        System.out.println(age+name);
     }
 
     public static void main(String[] args) {
-        local s1 = new local();
-        s1.display();
+        
+        System.out.println(local.companycode);
+
+        
+
+        // this was done by me, but above mentioned approach 
     }
     
 }
